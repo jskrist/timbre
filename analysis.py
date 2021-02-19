66eec314b49df8e2400e09dbaa4d78287d2be505
James Kristoff <jskrist@gmail.com>
2021-02-19 16:30:04 -0500

    - added plotting capabilities - added workaround for error in dpa/dea models - refactored some of the code

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 from timbre import run_state_pairs, f_to_c
 from pathlib import Path
 import astropy
+import matplotlib.pyplot as plt
 
 # shortpath to my model spec folder
 XIJA_PATH = "C:/Users/JKRIST~1/DOCUME~1/MATLAB/FOT_TO~2/THERMA~1/CHANDR~1.33/CHANDR~1/xija"
@@ -9,41 +10,44 @@ XIJA_PATH = "C:/Users/JKRIST~1/DOCUME~1/MATLAB/FOT_TO~2/THERMA~1/CHANDR~1.33/CHA
 PARAM_DTYPES = {'pitch': np.float64, 'roll': np.float64, 'fep_count': np.int8,
                 'ccd_count': np.int8, 'clocking': np.bool, 'vid_board': np.bool, 'sim_z': np.int32}
 # Properties no changing for any models in this analysis
-CONST_PARAMS = {'roll': np.float64(0.0), 'fep_count': np.int8(4), 'ccd_count': np.int8(4),
+CONST_PARAMS = {'roll': np.float64(0.0), 'fep_count': np.int8(4), 'ccd_count': np.int16(4),
                 'clocking': np.bool(True), 'vid_board': np.bool(True), 'sim_z': np.int32(100000)}
+# define the data unique to each model
+MODEL_INFO = {
+    'aacccdpt': {
+        'limit': -6.5, 'spec_file': Path(XIJA_PATH + '/aca/aca_spec.json'),
+        'model_init': {'aacccdpt': -6.5, 'aca0': -6.5, 'eclipse': False},
+        'params': []},
+    'pftank2t': {
+        'limit': f_to_c(110), 'spec_file': Path(XIJA_PATH + '/pftank2t/pftank2t_spec.json'),
+        'model_init': {'pftank2t': f_to_c(110), 'pf0tank2t': f_to_c(110), 'eclipse': False},
+        'params': ['roll']},
+    '1deamzt': {
+        'limit': 37.5, 'spec_file': Path(XIJA_PATH + '/dea/dea_spec.json'),
+        'model_init': {'1deamzt': 37.5, 'dea0': 37.5, 'eclipse': False, 'dpa_power': 0.0},
+        'params': ['roll', 'fep_count', 'ccd_count', 'clocking', 'vid_board', 'sim_z']},
+    '1dpamzt': {
+        'limit': 37.5, 'spec_file': Path(XIJA_PATH + '/dpa/dpa_spec.json'),
+        'model_init': {'1dpamzt': 37.5, 'dpa0': 37.5, 'eclipse': False, 'dpa_power': 0.0},
+        'params': ['roll', 'fep_count', 'ccd_count', 'clocking', 'vid_board', 'sim_z']}}
 
 
 def build_state_pairs(starting_pitch, model_params):
     """
     builds a list of state pairs.  These pairs vary in the destination pitch.
     """
-    return [({'pitch': starting_pitch, **model_params}, {'pitch': p, **model_params}) for p in range(45, 180, 5)]
+    return [({'pitch': starting_pitch, **model_params}, {'pitch': p, **model_params}) for p in range(40, 181, 5)]
 
 
-def find_balance_group(pitch, dwell_time, date):
+def find_balance_group(pitch, dwell_time, date, models=None):
     """
     determines the dwells that balance the given dwell/pitch combination as
     well as other dwells/pitches that are equivalent to the given inputs
     """
-    # define the data unique to each model
-    model_info = {
-        'aacccdpt': {
-            'limit': -6.5, 'spec_file': Path(XIJA_PATH + '/aca/aca_spec.json'),
-            'model_init': {'aacccdpt': -6.5, 'aca0': -6.5, 'eclipse': False},
-            'params': []},
-        'pftank2t': {
-            'limit': f_to_c(110), 'spec_file': Path(XIJA_PATH + '/pftank2t/pftank2t_spec.json'),
-            'model_init': {'pftank2t': f_to_c(110), 'pf0tank2t': f_to_c(110), 'eclipse': False},
-            'params': ['roll']},
-        '1deamzt': {
-            'limit': 37.5, 'spec_file': Path(XIJA_PATH + '/dea/dea_spec.json'),
-            'model_init': {'1deamzt': 37.5, 'dea0': 37.5, 'eclipse': False, 'dpa_power': 0.0},
-            'params': ['roll', 'fep_count', 'ccd_count', 'clocking', 'vid_board', 'sim_z']},
-        '1dpamzt': {
-            'limit': 37.5, 'spec_file': Path(XIJA_PATH + '/dpa/dpa_spec.json'),
-            'model_init': {'1dpamzt': 37.5, 'dpa0': 37.5, 'eclipse': False, 'dpa_power': 0.0},
-            'params': ['roll', 'fep_count', 'ccd_count', 'clocking', 'vid_board', 'sim_z']}}
-
+    if models is None:
+        model_info = MODEL_INFO
+    else:
+        model_info = {k: v for k, v in zip(MODEL_INFO.keys(), MODEL_INFO.values()) if k in models}
     results = []
     for model in model_info:
         # Here is the list of all the parameters needed for this model
@@ -69,12 +73,35 @@ def find_balance_group(pitch, dwell_time, date):
     return results
 
 
-results1 = find_balance_group(127, 10000, '2021:050:12:30:00')
-r1 = astropy.table.Table(results1[0])
-idx = r1['converged']
-r1 = r1[idx]
+def get_converged_table(results_data):
+    table_data = astropy.table.Table(results_data)
+    idx = table_data['converged']
+    return table_data[idx]
 
-results2 = find_balance_group(r1[0]['pitch2'], r1[0]['t_dwell2'], '2021:050:12:30:00')
-r2 = astropy.table.Table(results2[0])
-idx = r2['converged']
-r2 = r2[idx]
+
+PLOT = False
+msids = [k for k in MODEL_INFO.keys()]
+if PLOT:
+    fig, axs = plt.subplots(len(msids))
+
+results = {k: [] for k in msids}
+# the groups that balances the starting pitch/dwell for all models
+group_b = find_balance_group(127, 10000, '2021:050:12:30:00')
+for r_idx in range(0, len(msids)):
+    msid = msids[r_idx]
+    # the converged group that balances the starting pitch/dwell for the current model
+    g2 = get_converged_table(group_b[r_idx])
+    # the group that includes the starting pitch/dwell
+    group_a = find_balance_group(g2['pitch2'][0], g2['t_dwell2'][0], '2021:050:12:30:00', models=msid)
+    g1 = get_converged_table(group_a[0])
+    results[msid] = [g1, g2]
+    if PLOT:
+        axs[r_idx].plot(g2['pitch2'], g2['t_dwell2'], 'g.', g1['pitch2'], g1['t_dwell2'], 'y.')
+        axs[r_idx].set_title(msid)
+        axs[r_idx].set_xlabel('pitch')
+        axs[r_idx].set_ylabel('duration (s)')
+        axs[r_idx].set_xlim([40, 180])
+        axs[r_idx].set_ylim([-10000, 100000])
+
+if PLOT:
+    plt.show()
